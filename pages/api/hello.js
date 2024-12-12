38a2279d004475ae852da5ef46988834b7de807e
tanyarat.t <tanyarat.t@arise.tech>
2024-12-12 15:00:43 +0700

    Added console log

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
+  const ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'];
+  console.log('IP address:', ip);
+  console.log('All headers:', req.headers);
+
   res.status(200).json({ message: 'Hello from Vercel!' })
 }
